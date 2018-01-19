5a78fcc240625c052dfd17de01f081ba3c003ec0
severi <severikupari1@gmail.com>
2018-01-19 09:00:30 +0200

    Pohja viikko4

--- JavaApplication1/src/Viikko4/Tehtava1_vk4.java
+++ JavaApplication1/src/Viikko4/Tehtava1_vk4.java
@@ -2,6 +2,13 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+
+Laadi metodi (eli funktio), joka ottaa parametrinaan vuoden, kuukauden, p�iv�n ja tulostusformaatin.
+ Metodi tulostaa p�iv�nm��r�n halutussa formaatissa. Alla on mahdolliset formaattivaihtoehdot:
+
+6. joulukuuta 1917 (normaali formaatti)
+6.12.1917 (lyhyt formaatti esimerkiksi taulukoihin)
+1917-12-06 (kansainv�linen formaatti) 
  */
 package Viikko4;
 
@@ -11,4 +18,29 @@ package Viikko4;
  */
 public class Tehtava1_vk4 {
     
+    public static void pvmTulostus(String v,String kk,String pv,int formaatti ){
+        
+        
+        
+        switch (formaatti) {
+            case 1:
+                
+                break;
+            case 2:
+                
+                break;
+            case 3:
+                
+                break;
+            default:
+                throw new AssertionError();
+        }
+ 
+    }
+    
+    
+    
+    public static void main(String[] args) {
+       
+    }
 }
