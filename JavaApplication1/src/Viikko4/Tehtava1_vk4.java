46716707df48c5b22878939c900000b43f24e167
severi <severikupari1@gmail.com>
2018-01-19 09:52:05 +0200

    tehtava1-viikko4

--- JavaApplication1/src/Viikko4/Tehtava1_vk4.java
+++ JavaApplication1/src/Viikko4/Tehtava1_vk4.java
@@ -12,28 +12,49 @@ Laadi metodi (eli funktio), joka ottaa parametrinaan vuoden, kuukauden, p�iv�
  */
 package Viikko4;
 
+import java.time.MonthDay;
+import java.time.YearMonth;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+
+
 /**
  *
  * @author Severi
+ * 
+ * YearMonth yearMonthObject = YearMonth.of(vuosiluku, i+1);
+                daysInMonth = yearMonthObject.lengthOfMonth(); 
+                System.out.printf("%s %d\n",kk[i],daysInMonth);
+ * 
  */
 public class Tehtava1_vk4 {
     
-    public static void pvmTulostus(String v,String kk,String pv,int formaatti ){
+    public static void pvmTulostus(int v,int kk,int pv,int formaatti ){
+        String[] kkstring = {"Tammikuu","Helmikuu","Maaliskuu","Huhtikuu","Toukokuu","Kesäkuu","Heinäkuu","Elokuu","Syyskuu","Lokakuu","Marraskuu","Joulukuu"};
         
-        
+        YearMonth aika = YearMonth.of(v, kk);
         
         switch (formaatti) {
             case 1:
-                
+                System.out.printf("%d. %s %d\n",pv,kkstring[kk],v);
                 break;
             case 2:
-                
+                System.out.printf("%d.%d.%d\n",pv,kk,v);
                 break;
             case 3:
+                //System.out.printf("%d-%d-%d",v,kk,pv);
+                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM");
+                MonthDay source;
+                source = MonthDay.of(kk, pv);
+                
+                DateTimeFormatter pvm = DateTimeFormatter.ofPattern("-dd", Locale.ENGLISH);
+                
+                System.out.println(formatter.format(aika) + source.format(pvm));
                 
                 break;
             default:
-                throw new AssertionError();
+                System.out.println("Virhe formaatissa");
         }
  
     }
@@ -41,6 +62,9 @@ public class Tehtava1_vk4 {
     
     
     public static void main(String[] args) {
-       
+      // pvmTulostus(int v,int kk,int pv,int formaatti )
+        pvmTulostus(2018, 1, 6, 1);
+        pvmTulostus(2018, 1, 6, 2);
+        pvmTulostus(2018, 1, 6, 3);
     }
 }
