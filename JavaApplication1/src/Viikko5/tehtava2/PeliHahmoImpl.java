43f8ba998f2948a045650544a6450a58bf15e4e2
severi <severikupari1@gmail.com>
2018-01-26 08:27:28 +0200

    asd

--- JavaApplication1/src/Viikko5/tehtava2/PeliHahmoImpl.java
+++ JavaApplication1/src/Viikko5/tehtava2/PeliHahmoImpl.java
@@ -7,18 +7,11 @@ package Viikko5.tehtava2;
 
 
 public class PeliHahmoImpl  {
-
     public static void main(String[] args) {
-        PeliHahmo ukko1 = new PeliHahmo("severi", PeliHahmo.sukupuoli.mies, 0);
-        
-        
+        PeliHahmo ukko1 = new PeliHahmo("severi", PeliHahmo.sukupuoli.mies, 0);     
         System.out.println(ukko1.ase1.getVahinko());
         ukko1.ase1.setVahinko(Ase.Vahinko.keskikova);
-        System.out.println(ukko1.ase1.getVahinko());
-        
-        
-        System.out.println(ukko1.toString());
-        
-    }
-    
+        System.out.println(ukko1.ase1.getVahinko());    
+        System.out.println(ukko1.toString());     
+    }    
 }
