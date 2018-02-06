5fd24e3e2398761481590681da946f00bd404603
severi <severikupari1@gmail.com>
2018-02-06 10:00:11 +0200

    Video class ready

--- JavaApplication1/src/viikko8_tehtava2/Video.java
+++ JavaApplication1/src/viikko8_tehtava2/Video.java
@@ -8,7 +8,58 @@ package viikko8_tehtava2;
 /**
  *
  * @author s704383
+ * - director: String
+
+- language: String
+
+- playingTime: Integer
+
+- available: Boolean
+
+- comment: String
+
++ Video (title: String, director: String, language: String, time: Integer)
+
++ setComment (comment: String)
+
++ getAvailable(): Boolean
+
++ print()
  */
 public class Video {
+       private String director;
+       private String language;
+       private int playingTime;
+       private boolean available;
+       private String comment;
+       private String title;
+
+    public Video(String title, String director, String language, int playingTime, boolean available, String comment) {
+        this.title = title;
+        this.director = director;
+        this.language = language;
+        this.playingTime = playingTime;
+        this.available = available;
+        this.comment = comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public boolean isAvailable() {
+        return available;
+    }
+
+    public String Print() {
+        return "Video{" + "director=" + director + ", language=" + language + ", playingTime=" + playingTime + ", available=" + available + ", comment=" + comment + ", title=" + title + '}';
+    }
+
     
+    
+    
+       
+       
+       
+       
 }
