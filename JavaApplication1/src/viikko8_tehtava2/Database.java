0a0b8438390d2aac55de6d86476ff6af021a621a
severi <severikupari1@gmail.com>
2018-02-06 09:55:22 +0200

    Tallennus

---
+++ JavaApplication1/src/viikko8_tehtava2/Database.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package viikko8_tehtava2;
+
+/**
+ *
+ * @author s704383
+ */
+public class NewClass {
+    
+}
