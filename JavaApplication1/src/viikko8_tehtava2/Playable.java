f967fe6b7dd2f8a96e3df1e1faca488ea2c654cc
severi <severikupari1@gmail.com>
2018-02-06 10:25:37 +0200

    asd

--- JavaApplication1/src/viikko8_tehtava2/Playable.java
+++ JavaApplication1/src/viikko8_tehtava2/Playable.java
@@ -5,11 +5,19 @@
  */
 package viikko8_tehtava2;
 
+
+
 /**
  *
  * @author s704383
  */
 public abstract class Playable {
+
+    private CD soitettavat = new CD("asd", "asd", 2, true, "asd", "asd");
+    private Video video = new Video("asd", "asdasd", "asd", 0, true, "asds");
+    
+    
+    
     
     
     
