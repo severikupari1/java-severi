a200a323cd35292893e67c196edf763331de83d1
severi <severikupari1@gmail.com>
2018-02-15 13:16:55 +0200

    asd

--- JavaApplication1/src/thread/Laite.java
+++ JavaApplication1/src/thread/Laite.java
@@ -9,9 +9,41 @@ package thread;
  *
  * @author s704383
  */
-public abstract class Laite {
+public abstract class Laite implements ILaite{
     protected String tunnus;
     protected String hankintapaiva;
     protected String nimi;
+
+    public Laite(String tunnus, String hankintapaiva, String nimi) {
+        this.tunnus = tunnus;
+        this.hankintapaiva = hankintapaiva;
+        this.nimi = nimi;
+    }
+
+    public String getTunnus() {
+        return tunnus;
+    }
+
+    public void setTunnus(String tunnus) {
+        this.tunnus = tunnus;
+    }
+
+    public String getHankintapaiva() {
+        return hankintapaiva;
+    }
+
+    public void setHankintapaiva(String hankintapaiva) {
+        this.hankintapaiva = hankintapaiva;
+    }
+
+    public String getNimi() {
+        return nimi;
+    }
+
+    public void setNimi(String nimi) {
+        this.nimi = nimi;
+    }
+
+   
     
 }
--- JavaApplication1/src/thread/SahkoLaite.java
+++ JavaApplication1/src/thread/SahkoLaite.java
@@ -9,6 +9,34 @@ package thread;
  *
  * @author s704383
  */
-public class SahkoLaite {
+public abstract class SahkoLaite extends Laite{
+
+   
+    private double virrankulutus;
+    private boolean paalla;
+
+    public SahkoLaite(double virrankulutus, boolean paalla, String tunnus, String hankintapaiva, String nimi) {
+        super(tunnus, hankintapaiva, nimi);
+        this.virrankulutus = virrankulutus;
+        this.paalla = paalla;
+    }
+
+    public double getVirrankulutus() {
+        return virrankulutus;
+    }
+
+    public void setVirrankulutus(double virrankulutus) {
+        this.virrankulutus = virrankulutus;
+    }
+
+    public boolean isPaalla() {
+        return paalla;
+    }
+
+    public void setPaalla(boolean paalla) {
+        this.paalla = paalla;
+    }
+    
+    
     
 }
