661eced75813352db093811000bfb7755069bee4
Severi <32543075+Severi@users.noreply.github.com>
2018-02-16 09:51:55 +0200

    tallennus

--- JavaApplication1/src/thread/Main.java
+++ JavaApplication1/src/thread/Main.java
@@ -5,6 +5,7 @@
  */
 package thread;
 
+import java.util.ArrayList;
 import java.util.Date;
 
 /**
@@ -16,6 +17,20 @@ public class Main {
         Date date = new Date();
         
         System.out.println(date.toString());
+        
+        ArrayList<Laite> lista = new ArrayList<>();
+        
+        Potkulauta lauta = new Potkulauta(12, "tunnus", "15-02-2018", "lauta-2");
+        SahkoPatteri patteri = new SahkoPatteri(12, 22, true, "tunnus", "14-02-2018", "nimi");
+        Sahkovatkain vatkain = new Sahkovatkain(22, 100, 12, true, "tunnus", "13-02-2018", "vatkain");
+        
+        lista.add(lauta);
+        lista.add(patteri);
+        lista.add(vatkain);
+        
+        lista.;
+        
+        
         
     }
 }
