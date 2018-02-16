65f53127edd1f24061ddafaa4a71b0db512fb655
Severi <32543075+Severi@users.noreply.github.com>
2018-02-16 09:52:17 +0200

    asd

--- JavaApplication1/src/thread/Sahkovatkain.java
+++ JavaApplication1/src/thread/Sahkovatkain.java
@@ -9,6 +9,33 @@ package thread;
  *
  * @author Severi
  */
-public class Sahkovatkain {
+public class Sahkovatkain extends SahkoLaite{
+
+    public Sahkovatkain(double maxkierrosnopeus, double aanenvoimakkuus, double virrankulutus, boolean paalla, String tunnus, String hankintapaiva, String nimi) {
+        super(virrankulutus, paalla, tunnus, hankintapaiva, nimi);
+        this.maxkierrosnopeus = maxkierrosnopeus;
+        this.aanenvoimakkuus = aanenvoimakkuus;
+    }
+
+    public double getMaxkierrosnopeus() {
+        return maxkierrosnopeus;
+    }
+
+    public void setMaxkierrosnopeus(double maxkierrosnopeus) {
+        this.maxkierrosnopeus = maxkierrosnopeus;
+    }
+
+    public double getAanenvoimakkuus() {
+        return aanenvoimakkuus;
+    }
+
+    public void setAanenvoimakkuus(double aanenvoimakkuus) {
+        this.aanenvoimakkuus = aanenvoimakkuus;
+    }
+
+    
+    
+    private double maxkierrosnopeus;
+    private double aanenvoimakkuus;
     
 }
