3184612ac1676a85cc8d9f5dbeb7017892b7fda6
severi <severikupari1@gmail.com>
2018-04-10 10:40:39 +0300

    add

---
+++ JavaApplication11/src/User.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author vtkinju
+ */
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+public class User implements Comparable <User>{
+    private int id;
+    private String name;
+    private String password;
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+     public int compareTo(User a) {
+        return name.compareTo(a.name);
+    }
+
+     public String toString() {
+         return name + " " + password + " " + description;
+     }
+}
